663e72c81e56279a3ca30be97da3c33c6bbb962d
fmcfar200 <fmcfar200@caledonian.ac.uk>
2020-10-29 19:02:02 +0000

    Simple Before After Component

---
+++ before_after.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const element = React.createElement;
+
+class BeforeAfter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showBefore: true };
+  }
+
+  setShowBefore(value) {
+    this.setState({
+      showBefore: value,
+    });
+  }
+
+  render() {
+    let imageUrl;
+    let alt;
+    if (this.state.showBefore) {
+      imageUrl = this.props.before;
+      alt = "car before dent repair";
+    } else {
+      imageUrl = this.props.after;
+      alt = "car after dent repair";
+    }
+
+    return (
+      <div>
+        <div className="carousel-container">
+          <div className="numbertext"></div>
+          <img src={imageUrl} style={{ width: "100%" }} alt={alt} />
+          <div className="row">
+            <div className="image-carousel">
+              <button
+                className="before-btn demo cursor"
+                onClick={() => this.setShowBefore(true)}
+              >
+                Before
+              </button>
+              <button
+                className="after-btn demo cursor"
+                onClick={() => this.setShowBefore(false)}
+              >
+                After
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll(".before-after-container").forEach((domContainer) => {
+  // Read the comment ID from a data-* attribute.
+  const before = domContainer.dataset.before;
+  const after = domContainer.dataset.after;
+  ReactDOM.render(
+    element(BeforeAfter, {
+      before: before,
+      after: after,
+    }),
+    domContainer
+  );
+});
